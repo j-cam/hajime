d3bc5c948b176efa0473b952000da3da5e8b66ee
Jamie Campbell <jamie@spiritedcreative.com>
2015-09-30 14:20:03 -0700

    Adding basic nunjucks templates

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -4,13 +4,16 @@ var gulp = require('gulp');
 var config = require('../tasks.config.json');
 
 var newer = require('gulp-newer');
-
+var nunjucksRender = require('gulp-nunjucks-render');
 
 
 gulp.task('templates', function () {
 
-    return gulp.src(config.templates.files)
-      .pipe(newer(config.templates.dest))
-      .pipe(gulp.dest(config.templates.dest))
+
+  nunjucksRender.nunjucks.configure([config.templates.base]);
+  return gulp.src(config.templates.files)
+    .pipe(nunjucksRender())
+    .pipe(newer(config.templates.dest))
+    .pipe(gulp.dest(config.templates.dest))
 
 });
