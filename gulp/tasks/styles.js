9ebdabb1b72a9223dba37096e0f833bdbf5a22e7
Jamie Campbell <jamie@spiritedcreative.com>
2015-10-19 13:37:31 -0700

    Adds additional error handling to styles task.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var size = require('gulp-size');
 var config = require('../tasks.config.json');
 
+var gulpif = require('gulp-if');
 var sass = require('gulp-sass');
 var groupmq = require('gulp-group-css-media-queries');
 var autoprefixer = require('gulp-autoprefixer');
@@ -21,13 +22,15 @@ gulp.task('styles', function() {
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
-        }))
+        }).on('error', sass.logError))
         .pipe(sass({
             outputStyle: 'expanded'}
-        ))
-        .pipe(groupmq())
-        .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(
+        ).on('error', sass.logError))
+        // Combine media queries jacks up sourcemaps
+        // change the environment flag in config to run
+        .pipe( gulpif(config.environment.production, groupmq()))
+        .pipe( sourcemaps.write('../maps'))
+        .pipe( gulp.dest(
             config.styles.dest
         ))
         .pipe(size({ title: 'SIZE -> CSS', showFiles: true }))
