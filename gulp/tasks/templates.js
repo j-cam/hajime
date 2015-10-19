b6eb4599573a5334a196cb097be17633441ee069
Jamie Campbell <jamie@spiritedcreative.com>
2015-10-19 14:30:10 -0700

    Adds plumber errors handling to templates task

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -3,6 +3,9 @@
 var gulp = require('gulp');
 var config = require('../tasks.config.json');
 var gutil = require('gulp-util');
+
+var plumber = require('gulp-plumber');
+
 var newer = require('gulp-newer');
 var nunjucksRender = require('gulp-nunjucks-render');
 
@@ -10,10 +13,13 @@ var nunjucksRender = require('gulp-nunjucks-render');
 gulp.task('templates', function () {
 
 
+
   nunjucksRender.nunjucks.configure([config.templates.src]);
-  return gulp.src(config.templates.pages)
+
+  gulp.src(config.templates.pages)
+    .pipe(plumber())
     .pipe(nunjucksRender().on('error', gutil.log))
-    // .pipe(newer(config.templates.dest))
+    .pipe(plumber.stop())
     .pipe(gulp.dest('dist'))
 
 });
